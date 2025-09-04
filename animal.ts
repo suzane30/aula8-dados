9e0c57cc976874743f203469145cdd5323f591f3
jecaoliveira <jecaoliveira@gmail.com>
2025-09-04 17:07:25 -0300

    aula9 banco de dados2

--- animal.ts
+++ animal.ts
@@ -6,10 +6,23 @@ class Cachorro {
     public genero : String;
     public peso : String;
 
+    constructor (altura, ano, cor, raca, genero, peso){
+        this.altura = altura;
+        this.ano = ano;
+        this.cor = cor;
+        this.raca = raca;
+        this.genero = genero;
+        this.peso = peso;
+
+
+    }
+
     latir () {
+        console.log("auauauau")
 
     }
     correr () {
+        console.log("rumrumrur")
 
     }
     comer () {
@@ -24,3 +37,14 @@ class Cachorro {
 
 
 }
+
+const cachorro = new Cachorro ('grande', 2025, 'preto', 'pudou', 'maculino', 45)
+const cachorro1 = new Cachorro ('pequeno', 2024, 'marrom', 'pastoralemao',' feminino', 62 )
+const cachorro2 = new Cachorro ('gigante', 2022, 'preto', 'rotivale', 'masculino', 79)
+
+
+ cachorro.latir();
+ cachorro.correr();
+ console.log(cachorro.ano);
+
+ cachorro2.latir();
